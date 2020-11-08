c7c265a645075c7db4501f53faa37e3c2844f1cf
Tso, Peter <tsopeter@ku.edu>
2020-11-08 13:03:11 -0600

    Added Comments

--- testing/checkwin.js.test.js
+++ testing/checkwin.js.test.js
@@ -1,5 +1,13 @@
+
+/**
+*	@pre Must have valid board
+*	@post test the checkwin function
+*/
 function test_checkwin(){
+	//test name
 	let test = 'test_checkwin: ';
+	
+	//clear the board
 	test_clear();
 	
 	//
@@ -87,19 +95,40 @@ function test_checkwin(){
 	test_clear();
 }
 
+/**
+*	@param x Integer X position
+*	@param y Integer Y position
+*	@param color String color
+*	@pre must have valid coordinates
+*	@post assigns a color ot (x,y) of map
+*/
 function pmt(x, y, color){
 	map[x][y] = color;
 }
 
+/**
+*	@param f Function function that executes before test
+*	@param color String color
+*	@param expected Boolean expected result from checkwin
+*	@param m1 String test name
+*	@param m2 String message when successful
+*	@param m3 String message when error
+*	@pre must have valid function, color, and map
+*	@post test the function and logs the test
+*/
 function zt_test_checkwin(f, color, expected, m1, m2, m3){
 	let state = true;
+	//execute the function
 	f();
+	//check all position of map
 	for(let i = 0; i < map.length; i++){
 		for(let k = 0; k < map.length; k++){
 			for(let g = 0; g < 4; g++){
 				checkwin(i, k, color, mode[g]);
 			}
+			//if win detected,
 			if(flag){
+				//if the win is expected, log success message
 				if(flag == expected){
 					console.log(m1 + m2);
 					return;
@@ -107,13 +136,12 @@ function zt_test_checkwin(f, color, expected, m1, m2, m3){
 			}
 		}
 	}
+	//if win not detected and expected is false
 	if(flag == expected){
 		console.log(m1 + m2);
 	}
-	else if(flag == expected){
-		console.log(m1 + m2);
-	}
 	else{
+	//if the win is not detected and expected is true
 		console.log(m1 +m3);
 	}
 }
